fe33a3d75df9c6de60fb9ee04a8df773b1334b98
Oniqq600 <kunitsyn60@gmail.com>
2024-12-17 17:13:48 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/labstack/echo/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -16,6 +18,11 @@ type Message struct {
 	gorm.Model
 	Task   string `json:"task"`
 	IsDone bool   `json:"is_done"`
+}
+
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
 func InitDB() {
@@ -32,37 +39,136 @@ func InitDB() {
 	}
 }
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	var tasks []Message
-	w.Header().Set("Content-Type", "application/json")
-	err := db.Find(&tasks)
-	if err != nil {
-		fmt.Println("Всё ещё для того что бы не светило")
+func GetHandler(e echo.Context) error {
+	var messages []Message
+
+	newTable := db.Find(&messages)
+	if newTable == nil {
+		return e.JSON(http.StatusOK, &messages)
 	}
 
-	json.NewEncoder(w).Encode(tasks)
+	return e.JSON(http.StatusOK, &messages)
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		fmt.Fprintln(w, "Я не знаю нахрена здесь в начале буква W")
+func PostHandler(e echo.Context) error {
+	var messages Message
+
+	// Привязка входящих данных JSON к структуре Message
+	if err := e.Bind(&messages); err != nil {
+		return e.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Некорректный формат JSON",
+		})
 	}
 
-	var newTask Message
-	err := json.NewDecoder(r.Body).Decode(&newTask)
+	newTable := db.Create(&messages)
+	if newTable != nil {
+		fmt.Println("Меня так уже заебало обрабатывать эти ошибки ну когда это кончиться")
+	} else {
+		return e.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "GG я ливаю",
+		})
+	}
+
+	return e.JSON(http.StatusOK, Response{
+		Status:  "Success",
+		Message: "Запись успешно добавлена в базу данных",
+	})
+}
+
+var messages = make(map[int]Message)
+
+func PutchHendler(e echo.Context) error {
+	idParam := e.Param("id")
+	id, err := strconv.Atoi(idParam)
 	if err != nil {
-		fmt.Println("Это что бы крвсным не горело")
+		return e.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "BAD PIVO",
+		})
 	}
 
-	db.Create(&newTask)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newTask)
+	var upMessage Message
+
+	if err := e.Bind(&upMessage); err != nil {
+		return e.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Ну чё то ",
+		})
+	}
+
+	var message Message
+	result := db.First(&message, id)
+	// if result.Error != gorm.ErrRecordNotFound {
+	// 	return e.JSON(http.StatusNotFound, Response{
+	// 		Status:  "Error",
+	// 		Message: "Нет сообщения",
+	// 	})
+	// }
+
+	if upMessage.Task != "" {
+		message.Task = upMessage.Task
+	}
+	message.IsDone = upMessage.IsDone
+
+	result = db.Save(&message)
+
+	if result.Error != nil {
+		return e.JSON(http.StatusInternalServerError, Response{
+			Status:  "Error",
+			Message: "Ошибка при обновлении записи",
+		})
+	}
+
+	return e.JSON(http.StatusOK, Response{
+		Status:  "Success",
+		Message: "Запись успешно обновлена",
+	})
+
+}
+
+func DeletHendler(e echo.Context) error {
+	idParam := e.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Некорректный ID",
+		})
+	}
+
+	var message Message
+	result := db.First(&message, id)
+	if result.Error != nil {
+		return e.JSON(http.StatusNotFound, Response{
+			Status:  "Error",
+			Message: "Сообщение не найдено",
+		})
+	}
+
+	result = db.Delete(&message)
+	if result.Error != nil {
+		return e.JSON(http.StatusInternalServerError, Response{
+			Status:  "Error",
+			Message: "Ошибка при удалении записи",
+		})
+	}
+
+	return e.JSON(http.StatusOK, Response{
+		Status:  "Success",
+		Message: "Запись успешно удалена",
+	})
 }
 
 func main() {
 	InitDB()
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/post", PostHandler)
-	http.ListenAndServe("localhost:8080", nil)
+	e := echo.New()
+
+	e.GET("/messages", GetHandler)
+	e.POST("/messages", PostHandler)
+	e.PATCH("/messages/:id", PutchHendler)
+	e.DELETE("/messages/:id", DeletHendler)
+	e.Start("localhost:8080")
 
 }
