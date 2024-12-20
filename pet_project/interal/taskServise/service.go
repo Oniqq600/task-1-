af31c83d3a8eeefa92b7a9dda6fa2a3e375b72df
Oniqq600 <kunitsyn60@gmail.com>
2024-12-20 19:35:16 +0700

    Create service.go

---
+++ pet_project/interal/taskServise/service.go
@@ -0,0 +1,25 @@
+package orm
+
+type TaskService struct {
+	repo MessageRepository
+}
+
+func NewService(repo *taskRepository) *TaskService {
+	return &TaskService{repo: repo}
+}
+
+func (s *TaskService) CreateTask(task Message) (Message, error) {
+	return s.repo.CreateTask(task)
+}
+
+func (s *TaskService) GetAllTasks() ([]Message, error) {
+	return s.repo.GetAllTasks()
+}
+
+func (s *TaskService) UpdateTaskByID(id uint, task Message) (Message, error) {
+	return s.repo.UpdateTaskByID(id, task)
+}
+
+func (s *TaskService) DeleteTaskByID(id uint) error {
+	return s.repo.DeleteTaskByID(id)
+}
