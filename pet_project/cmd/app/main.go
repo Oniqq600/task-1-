6632b615a263a771dff2e42935f0a45dfb7825e1
Oniqq600 <kunitsyn60@gmail.com>
2025-01-01 23:47:25 +0700

    Update main.go

--- pet_project/cmd/app/main.go
+++ pet_project/cmd/app/main.go
@@ -5,22 +5,38 @@ import (
 	"pet_project/internal/hendlers"
 	orm "pet_project/internal/taskServise"
 
+	"github.com/labstack/echo/v4/middleware"
+
+	"pet_project/internal/web/tasks"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	database.InitDB()
+	if err := database.DB.AutoMigrate(&orm.Message{}); err != nil {
+		panic("Error")
+	}
+
 	repo := orm.NewTaskRepository(database.DB)
 	service := orm.NewService(repo)
 
-	hendler := hendlers.NewHendler(service)
+	handler := hendlers.NewHendler(service)
 
 	e := echo.New()
 
-	e.GET("/api/get", hendler.GetTaskHendler)
-	e.POST("/api/post", hendler.PostTaskHandler)
-	e.PATCH("/api/patch/:id", hendler.PutchTaskHendler)
-	e.DELETE("/api/delete/:id", hendler.DeleteTaskHendler)
-	e.Start("localhost:8080")
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
+	strictHandler := tasks.NewStrictHandler(handler, nil)
+	tasks.RegisterHandlers(e, strictHandler)
+
+	e.GET("/api/get", handler.GetTaskHendler)
+	e.POST("/api/post", handler.PostTaskHandler)
+	e.PATCH("/api/patch/:id", handler.PutchTaskHendler)
+	e.DELETE("/api/delete/:id", handler.DeleteTaskHendler)
+
+	if err := e.Start("localhost:8080"); err != nil {
+		panic("Error")
+	}
 }
