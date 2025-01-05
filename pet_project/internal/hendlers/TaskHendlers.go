0131a50d4e9559e826a0d133cd3407b2bf39eb80
Oniqq600 <kunitsyn60@gmail.com>
2025-01-05 15:46:48 +0700

    Add files via upload

---
+++ pet_project/internal/hendlers/TaskHendlers.go
@@ -0,0 +1,93 @@
+package handlers
+
+import (
+	"context"
+	orm "pet_project/internal/taskServise"
+	"pet_project/internal/web/tasks"
+)
+
+type Handler struct {
+	Service *orm.TaskService
+}
+
+func NewTaskHendler(service *orm.TaskService) *Handler {
+	return &Handler{
+		Service: service,
+	}
+}
+
+func (h *Handler) GetTasks(ctx context.Context, request tasks.GetTasksRequestObject) (tasks.GetTasksResponseObject, error) {
+	allTasks, err := h.Service.GetAllTasks()
+	if err != nil {
+		return nil, err
+	}
+
+	response := tasks.GetTasks200JSONResponse{}
+
+	for _, tsk := range allTasks {
+		task := tasks.Task{
+			Id:     &tsk.ID,
+			Task:   &tsk.Task,
+			IsDone: &tsk.IsDone,
+		}
+		response = append(response, task)
+	}
+
+	return response, nil
+}
+
+func (h *Handler) PostTasks(ctx context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
+	taskRequest := request.Body
+
+	taskToCreate := orm.Message{
+		Task:   *taskRequest.Task,
+		IsDone: *taskRequest.IsDone,
+	}
+	createdTask, err := h.Service.CreateTask(taskToCreate)
+
+	if err != nil {
+		return nil, err
+	}
+
+	response := tasks.PostTasks201JSONResponse{
+		Id:     &createdTask.ID,
+		Task:   &createdTask.Task,
+		IsDone: &createdTask.IsDone,
+	}
+
+	return response, nil
+}
+
+func (h *Handler) PatchTasksId(ctx context.Context, request tasks.PatchTasksIdRequestObject) (tasks.PatchTasksIdResponseObject, error) {
+	taskID := request.Id
+	taskRequest := request.Body
+
+	taskToUpdate := orm.Message{
+		Task:   *taskRequest.Task,
+		IsDone: *taskRequest.IsDone,
+	}
+
+	updatedTask, err := h.Service.UpdateTaskByID(uint(taskID), taskToUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	response := tasks.PatchTasksId200JSONResponse{
+		Id:     &updatedTask.ID,
+		Task:   &updatedTask.Task,
+		IsDone: &updatedTask.IsDone,
+	}
+
+	return response, nil
+}
+
+func (h *Handler) DeleteTasksId(ctx context.Context, request tasks.DeleteTasksIdRequestObject) (tasks.DeleteTasksIdResponseObject, error) {
+	taskID := request.Id
+
+	err := h.Service.DeleteTaskByID(uint(taskID))
+	if err != nil {
+		return nil, err
+	}
+
+	return &tasks.DeleteTasksId204Response{}, nil
+}
